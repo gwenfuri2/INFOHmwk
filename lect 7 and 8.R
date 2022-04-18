80c9852b6c60c21ad5fd6267d69d32cc81115cb2
GF <63945124+gwenfuri2@users.noreply.github.com>
2022-04-18 13:55:27 -0700

    Add files via upload

---
+++ lect 7 and 8.R
@@ -0,0 +1,17 @@
+#1) Read in the female baby names data file into a variable called `names`
+
+#2) Create a data frame `names_2013` that contains only the rows for the year 2013
+
+#3) write in a comment the question "What was the most popular female name in 2013?" and answer. 
+
+#4) Write a function `most_popular_in_year` that takes in a year as a value and returns the most popular name in that year
+
+#5) write in a comment "What was the most popular female name in 1994?" and answer
+
+#6) Write a function `number_in_million` that takes in a name and a year, and returns statistically how many babies out of 1 million born that year have that name. Hint: get the popularity percentage, and take that percentage out of 1 million.
+
+#7) write in a comment "How many babies out of 1 million had the name 'Laura' in 1995?" and answer 
+
+#8) write in a comment "How many babies out of 1 million had your name in the year you were born?" and answer 
+
+#9) think about the following: what does this tell you about how easy it is to identify you with just your name and birth year?
